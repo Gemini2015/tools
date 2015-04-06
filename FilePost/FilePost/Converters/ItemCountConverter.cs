d91ab8890034d44a58e37c9a423d7407fbb8a81e
Chris Cheng <gemini2015@hotmail.com>
2015-04-06 21:34:18 +0800

    add ItemCountConverter

---
+++ FilePost/FilePost/Converters/ItemCountConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
+
+namespace FilePost.Converters
+{
+    public class ItemCountConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            ObservableCollection<FPFile> list = (ObservableCollection<FPFile>)value;
+            return string.Format("Files: {0}", list.Count);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
